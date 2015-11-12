5d252c6f2a873129b2abdc804431b091af66bd81
Balint Vekerdy <balint@dangerfarms.com>
2015-11-12 18:08:57 +0100

    Add responseType to google options

--- src/oauth.js
+++ src/oauth.js
@@ -212,24 +212,31 @@ angular.module("oauth.providers", ["oauth.utils"])
                     var cordovaPluginList = cordova.require("cordova/plugin_list");
                     if($cordovaOauthUtility.isInAppBrowserInstalled(cordovaPluginList) === true) {
                         var redirect_uri = "http://localhost/callback";
+                        var responseType = "token";
                         if(options !== undefined) {
                             if(options.hasOwnProperty("redirect_uri")) {
                                 redirect_uri = options.redirect_uri;
                             }
+                            if(options.hasOwnProperty("responseType")) {
+                                responseType = options.responseType;
+                            }
                         }
-                        var browserRef = window.open('https://accounts.google.com/o/oauth2/auth?client_id=' + clientId + '&redirect_uri=' + redirect_uri + '&scope=' + appScope.join(" ") + '&approval_prompt=force&response_type=token', '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
+                        var urlSplitChar = (responseType === 'token') ? '#' : '?';
+                        var browserRef = window.open('https://accounts.google.com/o/oauth2/auth?client_id=' + clientId + '&redirect_uri=' + redirect_uri + '&scope=' + appScope.join(" ") + '&approval_prompt=force&response_type=' + responseType, '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
                         browserRef.addEventListener("loadstart", function(event) {
                             if((event.url).indexOf(redirect_uri) === 0) {
                            		browserRef.removeEventListener("exit",function(event){});
                             	browserRef.close();
-                                var callbackResponse = (event.url).split("#")[1];
+                                var callbackResponse = (event.url).split(urlSplitChar)[1];
                                 var responseParameters = (callbackResponse).split("&");
                                 var parameterMap = [];
                                 for(var i = 0; i < responseParameters.length; i++) {
                                     parameterMap[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
                                 }
-                                if(parameterMap.access_token !== undefined && parameterMap.access_token !== null) {
+                                if(responseType === 'token' && parameterMap.access_token !== undefined && parameterMap.access_token !== null) {
                                     deferred.resolve({ access_token: parameterMap.access_token, token_type: parameterMap.token_type, expires_in: parameterMap.expires_in });
+                                } else if (responseType === 'code' && parameterMap.code !== undefined && parameterMap.code !== null) {
+                                    deferred.resolve({ code: parameterMap.code });
                                 } else {
                                     deferred.reject("Problem authenticating");
                                 }
