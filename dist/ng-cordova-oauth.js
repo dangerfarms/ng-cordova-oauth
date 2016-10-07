ef89412d264841eb52aefef4b59dbd93c6b4b540
Balint Vekerdy <balint@dangerfarms.com>
2016-10-07 11:37:56 +0100

    Build changes to allow overriding response_type param in google

--- dist/ng-cordova-oauth.js
+++ dist/ng-cordova-oauth.js
@@ -779,24 +779,31 @@
       if(window.cordova) {
         if($cordovaOauthUtility.isInAppBrowserInstalled()) {
           var redirect_uri = "http://localhost/callback";
+          var response_type = "token id_token";
           if(options !== undefined) {
             if(options.hasOwnProperty("redirect_uri")) {
               redirect_uri = options.redirect_uri;
             }
+            if(options.hasOwnProperty("response_type")) {
+              response_type = options.response_type;
+            }
           }
-          var browserRef = window.cordova.InAppBrowser.open('https://accounts.google.com/o/oauth2/auth?client_id=' + clientId + '&redirect_uri=' + redirect_uri + '&scope=' + appScope.join(" ") + '&approval_prompt=force&response_type=token id_token', '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
+          var urlSplitChar = (response_type === 'token') ? '#' : '?';
+          var browserRef = window.cordova.InAppBrowser.open('https://accounts.google.com/o/oauth2/auth?client_id=' + clientId + '&redirect_uri=' + redirect_uri + '&scope=' + appScope.join(" ") + '&approval_prompt=force&response_type=' + response_type, '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
           browserRef.addEventListener("loadstart", function(event) {
             if((event.url).indexOf(redirect_uri) === 0) {
               browserRef.removeEventListener("exit",function(event){});
               browserRef.close();
-              var callbackResponse = (event.url).split("#")[1];
+              var callbackResponse = (event.url).split(urlSplitChar)[1];
               var responseParameters = (callbackResponse).split("&");
               var parameterMap = [];
               for(var i = 0; i < responseParameters.length; i++) {
                 parameterMap[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
               }
-              if(parameterMap.access_token !== undefined && parameterMap.access_token !== null) {
+              if(response_type === "token" && parameterMap.access_token !== undefined && parameterMap.access_token !== null) {
                 deferred.resolve({ access_token: parameterMap.access_token, token_type: parameterMap.token_type, expires_in: parameterMap.expires_in, id_token: parameterMap.id_token });
+              } else if (response_type === 'code' && parameterMap.code !== undefined && parameterMap.code !== null) {
+                deferred.resolve({ code: parameterMap.code });
               } else {
                 deferred.reject("Problem authenticating");
               }
